59f6144eed10a0f37a22ef3d362f3d9da4c9c6be
Yeo <guswls9281@gmail.com>
2022-06-23 09:37:41 +0900

    Update mysql_connection.py

--- mysql_connection.py
+++ mysql_connection.py
@@ -6,7 +6,7 @@ def get_connection():
         # host 를 써야, 접속할수가 있다. mysql참고 혹은 aws 참고
         database = 'flask_movie_db',
         # mysql의 스키마 이름
-        user = 'movie_user1',
+        user = 'movie_user2',
         password = '1234hello7'
     )
     return connection

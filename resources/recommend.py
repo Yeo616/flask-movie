642dc4f448a3efca09e57f995015b13e079df283
Yeo <guswls9281@gmail.com>
2022-06-23 13:05:10 +0900

    .

---
+++ resources/recommend.py
@@ -0,0 +1,9 @@
+import datetime
+from http import HTTPStatus
+from os import access
+from flask import request
+from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, jwt_required
+from flask_restful import Resource
+from mysql.connector.errors import Error
+from mysql_connection import get_connection
+import mysql.connector
